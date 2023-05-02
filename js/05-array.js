837324a83e58e692ec7146eeee085f38c9c8753a
Rimantas <belauzas@yahoo.com>
2023-05-02 10:18:52 +0300

    Array

---
+++ js/05-array.js
@@ -0,0 +1,47 @@
+console.clear();
+/*
+ARRAY - arėjus (slang), sąrašas, listas(slang), matrica, masyvas.
+*/
+
+const empty = [];
+console.log(empty);
+
+//                 0  1  2  3  4
+const pazymiai = [10, 2, 8, 4, 6];
+console.log(pazymiai);
+
+// stengtis isvengti tokio atvejo
+const random = [1, 'antras', 3, '4', 3.14, 'asdasd'];
+
+//                  0         1        2       3
+const vardai = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+console.log(vardai);
+
+const vardas1 = vardai[0];
+console.log(vardas1);
+
+const vardas2 = vardai[1];
+console.log(vardas2);
+
+console.log('--------------');
+console.log(vardai[0]);
+console.log(vardai[1]);
+console.log(vardai[2]);
+console.log(vardai[3]);
+console.log(vardai[4]);
+console.log(vardai[10000000]);
+console.log(vardai[-1]);
+console.log(vardai[1.5]);
+
+console.log('--------------');
+
+//              0  1  2  3  4
+const marks = [10, 2, 8, 4, 6];
+console.log(marks);
+
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+console.log(marks[5]);
