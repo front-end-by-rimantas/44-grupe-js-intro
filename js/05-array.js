3afdf8d8ef2a0c89a6e84c9d1dba3ead3e7b38ec
Rimantas <belauzas@yahoo.com>
2023-05-02 11:44:39 +0300

    Array index, length and strings alike

--- js/05-array.js
+++ js/05-array.js
@@ -1,6 +1,10 @@
 console.clear();
 /*
 ARRAY - arėjus (slang), sąrašas, listas(slang), matrica, masyvas.
+
+Masyvas prasideda index = 0;
+array.length - masyve esanciu reiksmiu kiekis;
+
 */
 
 const empty = [];
@@ -45,3 +49,90 @@ console.log(marks[2]);
 console.log(marks[3]);
 console.log(marks[4]);
 console.log(marks[5]);
+
+console.clear();
+
+const marks1 = [10, 9, 8];
+const marks2 = [6, 7, 8];
+const marks3 = [5, 5, 5];
+
+function average(marks) {
+    let sum = 0;
+    sum += marks[0];
+    sum += marks[1];
+    sum += marks[2];
+
+    return sum / marks.length;
+}
+
+console.log('Vidurkis 1:', marks1, '=', average(marks1));
+console.log('Vidurkis 2:', marks2, '=', average(marks2));
+console.log('Vidurkis 3:', marks3, '=', average(marks3));
+
+console.clear();
+
+const dict1 = ['pirmas', 'zodis', 'labas'];
+const dict2 = ['Labas', 'rytas', 'Lietuva'];
+const dict3 = ['Labas', 'rytas', 'suraitytas'];
+
+// Zodynas: a, b, c.
+
+function dictionary(words) {
+    let result = 'Zodynas: ';
+
+    result += words[0] + ', ';
+    result += words[1] + ', ';
+    result += words[2] + '.';
+
+    return result;
+}
+
+console.log(dictionary(dict1));
+console.log(dictionary(dict2));
+console.log(dictionary(dict3));
+
+console.clear();
+
+console.log(7 + 5);
+console.log(7 - 5);
+console.log(7 / 5);
+console.log(7 % 5);
+
+console.log('labas' + 'rytas');
+console.log(777 + 'rytas');
+console.log('labas' + 888);
+
+console.log('labas' - 888);
+console.log('labas' * 2);
+console.log('labas' / 2);
+console.log('labas' % 2);
+console.log('labas' * 'rytas');
+
+console.log(2 ** 3);
+console.log(9 ** 2);
+console.log(9 ** 0.5);
+
+console.clear();
+
+const text = 'Labas rytas';
+console.log(text);
+console.log(text[-1]);
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text[3]);
+console.log(text[4]);
+console.log(text[5]);
+console.log(text[6]);
+console.log(text[7]);
+console.log(text[8]);
+console.log(text[9]);
+console.log(text[10]);
+console.log(text[1100000000]);
+
+console.log(text.length);
+console.log(''.length);
+console.log(' '.length);
+console.log('  '.length);
+console.log('   '.length);
+
