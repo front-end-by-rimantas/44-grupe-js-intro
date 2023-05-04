60795d98b6c5ab4c51c09f439d357b54df7a8922
Rimantas <belauzas@gmail.com>
2023-05-04 11:40:23 +0300

    Ternary

---
+++ js/11-ternary.js
@@ -0,0 +1,55 @@
+console.clear();
+/*
+TERNARY
+
+klausimas ? teigiama reiksme : neigiama reiksme;
+*/
+
+const leidimas = 100 >= 18 ? 'pilnametis' : 'nepilnametis';
+console.log(leidimas);
+
+let approve = '';
+
+if (100 >= 18) {
+    approve = 'pilnametis';
+} else {
+    approve = 'nepilnametis';
+}
+console.log(approve);
+
+
+const a = 1 
+            ? 2
+            : 3;
+console.log(a);
+
+const b = 1
+            ? 2
+            : 3
+                ? 4
+                : 5;
+console.log(b);
+
+const c = 1 < 2
+                ? 3 
+                    ? 4 
+                    : 5 
+                : 6;
+console.log(c);
+
+const d = 1 > 2
+                ? 3 
+                : 4 
+                    ? 5 
+                    : 6;
+console.log(d);
+
+const e = 1 < 2 
+                ? 3 
+                    ? 4 
+                    : 5 
+                : 6 
+                    ? 7 
+                    : 8;
+console.log(e);
+
