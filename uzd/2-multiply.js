a68065ddee608b54b2f8744f7af2a73243e3acb4
Rimantas <belauzas@gmail.com>
2023-05-09 10:13:39 +0300

    Update

--- uzd/2-multiply.js
+++ uzd/2-multiply.js
@@ -22,26 +22,26 @@ function multiply(a, b) {
     return result;
 }
 
+// tipai
 console.log(multiply('labas', 5));
 console.log(multiply(5, 'labas'));
 console.log(multiply('labas', 'rytas'));
 console.log(multiply(7, false));
 console.log(multiply(true, 5));
+
+// ne skaicius
 console.log(multiply(Infinity, 5));
 console.log(multiply(5, Infinity));
 console.log(multiply(Infinity, Infinity));
 console.log(multiply(-Infinity, 5));
 console.log(multiply(5, -Infinity));
 console.log(multiply(-Infinity, -Infinity));
+
 console.log(multiply(NaN, 5));
 console.log(multiply(5, NaN));
 console.log(multiply(NaN, NaN));
 
-console.log(multiply(7, 5));
-console.log(multiply(-7, 5));
-console.log(multiply(-7, -5));
-console.log(multiply(7, -5));
-
+// ne tipai
 console.log(multiply([], 1));
 console.log(multiply([], []));
 console.log(multiply(2, []));
@@ -53,3 +53,9 @@ console.log(multiply(2, {}));
 console.log(multiply(multiply, 1));
 console.log(multiply(multiply, multiply));
 console.log(multiply(2, multiply));
+
+// ok
+console.log(multiply(7, 5));
+console.log(multiply(-7, 5));
+console.log(multiply(-7, -5));
+console.log(multiply(7, -5));
