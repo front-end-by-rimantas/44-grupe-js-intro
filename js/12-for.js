dd8551274e878002878b3ca1a687b7017033e72a
Rimantas <belauzas@gmail.com>
2023-05-09 09:10:09 +0300

    For, empty func

--- js/12-for.js
+++ js/12-for.js
@@ -41,3 +41,74 @@ for (let i = 0; i < pazymiai.length; i++) {
 }
 
 console.log('END');
+
+console.clear();
+
+console.log('5', parseInt('5'));
+console.log(typeof '5', typeof parseInt('5'));
+
+console.log('3.14', parseInt('3.14'));
+console.log('3.14', parseFloat('3.14'));
+console.log('777', parseFloat('777'));
+
+console.log('labas', parseInt('labas'));
+console.log('labas', parseFloat('labas'));
+
+console.log('Infinity', parseFloat('Infinity'));
+console.log('-Infinity', parseFloat('-Infinity'));
+console.log('NaN', parseFloat('NaN'));
+
+console.log('--------------');
+console.log('5', +'5');
+console.log('3.14', +'3.14');
+console.log('labas', +'labas');
+console.log('Infinity', +'Infinity');
+console.log('NaN', +'NaN');
+
+console.log('--------------');
+console.log('5', parseInt('5'), parseFloat('5'), +'5');
+console.log('3.14', parseInt('3.14'), parseFloat('3.14'), +'3.14');
+console.log('-3.14', parseInt('-3.14'), parseFloat('-3.14'), +'-3.14');
+console.log('1/3', parseInt('1/3'), parseFloat('1/3'), +'1/3');
+console.log('123abc', parseInt('123abc'), parseFloat('123abc'), +'123abc');
+console.log('abc123', parseInt('abc123'), parseFloat('abc123'), +'abc123');
+console.log('213', +'213', -'213');
+console.log('-213', +'-213', -'-213');
+
+console.log('--------------');
+
+for (let i = 0; i < 2; i += 0.1) {
+    // let suapvalintas = +i.toFixed(3);
+    let suapvalintas = parseFloat(i.toFixed(3));
+    suapvalintas += 1;
+    console.log(suapvalintas, '-', i);
+}
+
+console.log('2eur', parseFloat('2eur'), +'2eur');
+console.log('$5', parseFloat('$5'), +'$5');
+
+console.clear();
+
+const text = 'Labas!';
+
+for (let i = 0; i < text.length; i++) {
+    console.log(i, '-', text[i]);
+}
+
+console.log('------');
+
+for (let i = text.length - 1; i >= 0; --i) {
+    console.log(i, '-', text[i]);
+}
+
+console.log('------');
+let atbulas = '';
+
+for (let i = 0; i < text.length; i++) {
+    const index = text.length - i - 1;
+    const symbol = text[index];
+    atbulas += symbol;
+    console.log(i, '-', symbol);
+}
+
+console.log('>>>', atbulas);
